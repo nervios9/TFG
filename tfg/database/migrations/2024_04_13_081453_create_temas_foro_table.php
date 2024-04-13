f88414f77bd515497aa761e17b9c5f0ce16963bf
nervios9 <sergiogomez1999@hotmail.com>
2024-04-13 10:26:56 +0200

    añadir tablas

---
+++ tfg/database/migrations/2024_04_13_081453_create_temas_foro_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('temas', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('contenido');
+            $table->date('fecha_creacion');
+            $table->foreignId('usuario_id')->constrained();
+            $table->integer('categoria_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('temas_foro');
+    }
+};
---
+++ tfg/database/migrations/2024_04_13_081512_create_respuestas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('respuestas', function (Blueprint $table) {
+            $table->id();
+            $table->string('contenido');
+            $table->date('fecha_creacion');
+            $table->foreignId('usuario_id')->constrained();
+            $table->foreignId('temas_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('respuestas');
+    }
+};
