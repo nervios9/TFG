06ffd7ec5b9dd42e3cee56d0c8fec605086c43af
nervios9 <sergiogomez1999@hotmail.com>
2024-05-18 17:59:44 +0200

    ajustar el menu del dashboard

--- tfg/resources/views/dashboard.blade.php
+++ tfg/resources/views/dashboard.blade.php
@@ -4,15 +4,10 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" >
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" ></script>
 <script src="{{ asset('js/app.js') }}" defer></script>
-
+@include('header')
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-          
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
+   
+        
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
