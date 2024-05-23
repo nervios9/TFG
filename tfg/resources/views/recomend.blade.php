74e8ab0bd1b358f1fd9f0fed1ed6114e328c891e
nervios9 <sergiogomez1999@hotmail.com>
2024-05-23 08:40:01 +0200

    actualizar portadas

--- tfg/resources/views/recomend.blade.php
+++ tfg/resources/views/recomend.blade.php
@@ -5,7 +5,7 @@
     @if ($libro)
     <td class="imagenesLibros"><a href="{{route('show',[$libro->id])}}"><img style="height: 300px"  src="{{url('images/libros/' .$libro->imagen)}}" alt="Portada Libro" onerror="this.src='{{ url('images/libros/WIP.png') }}'"></a></td>
 @else
-    <p>No se encontró ningún libro para la opción seleccionada.</p>
+    <p>No se encontró ningún libro para la opción seleccionadas.</p>
 @endif
 </div>
 <a class="d-flex justify-content-center align-items-center centered-div" href="{{url('/recomendador')}}">Hacer otra consulta</a>
